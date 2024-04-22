073d451232d4f237d0da7d3a0fb9781cc81f81e3
OmJRamdas <93028735+OmJRamdas@users.noreply.github.com>
2024-04-22 14:41:29 +0800

    Update tls-alex-client-sdl.cpp

--- TLS_client/tls-alex-client-sdl.cpp
+++ TLS_client/tls-alex-client-sdl.cpp
@@ -1,5 +1,5 @@
 // To Compile
-// g++ tls-alex-client-sdl.cpp make_tls_client.cpp tls_client_lib.cpp tls_pthread.cpp tls_common_lib.cpp -pthread -lssl -lcrypto -lSDL2 -o tls-alex-client
+// g++ tls-alex-client-sdl.cpp make_tls_client.cpp tls_client_lib.cpp tls_pthread.cpp tls_common_lib.cpp -pthread -lssl -lcrypto -lSDL2 -o tls-alex-client-final
 
 // To make c++ code work
 #include <iostream>
