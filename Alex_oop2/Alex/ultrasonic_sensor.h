7f6fa5c2fc09450971f3de402686d54f57e8e997
KuanHsienn <141622026+KuanHsienn@users.noreply.github.com>
2024-04-25 10:43:18 +0800

    Update ultrasonic_sensor.h

--- Alex_oop2/Alex/ultrasonic_sensor.h
+++ Alex_oop2/Alex/ultrasonic_sensor.h
@@ -6,8 +6,8 @@
 class Ultra
 {
 private:
-	byte _echo;	// yellow // PD1 // PA2
-	byte _trig;	// orange // PD0 // PA3
+	byte _echo;	// yellow // PD1 
+	byte _trig;	// orange // PD0 
 public:	
 	Ultra() {};                   // do not use
 	Ultra(byte echo, byte _trig);
